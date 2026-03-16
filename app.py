b62f53faf4256cba0c620c7726135c54c673cc1f
Abhisheksinghpawar <abhisheksinghpawar17@gmail.com>
2026-03-15 22:02:18 -0500

    Refactor imports in app.py

--- app.py
+++ app.py
@@ -20,10 +20,9 @@ Files:
 """
 
 import streamlit as st
-from phi.agent import Agent, RunResponse
+from phi.agent import Agent
 from phi.model.google import Gemini
 from phi.tools.duckduckgo import DuckDuckGo
-from google.generativeai import upload_file, get_file
 import google.generativeai as genai
 import re
 from youtube_transcript_api import (
